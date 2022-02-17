3948ef3c990e684140461957b9ee12a976240e09
roeishy <roeishy3@gmail.com>
2022-02-17 15:45:22 +0200

    update header

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -10,6 +10,8 @@ function renderGallery() {
     document.querySelector('.memes-gallery').classList.add("hide")
     document.querySelector('.editor').classList.add("hide")
     document.querySelector('.gallery').classList.remove("hide")
+    document.querySelector('#memes-gallery-btn').classList.remove("active")
+    document.querySelector('#gallery-btn').classList.add("active")
     const imgs = getImgs(gFilterBy);
     var strHTML = imgs.map(img =>
         `<div class="card"><img class="img${img.id}" src="${img.url}" onclick="onImg(${img.id})" /></div>`
@@ -33,6 +35,8 @@ function renderMemesGallery() {
     document.querySelector('.gallery').classList.add("hide")
     document.querySelector('.editor').classList.add("hide")
     document.querySelector('.memes-gallery').classList.remove("hide")
+    document.querySelector('#gallery-btn').classList.remove("active")
+    document.querySelector('#memes-gallery-btn').classList.add("active")
     const savedMemes = getSavedMemes();
     var strHTML = savedMemes.map(savedMeme =>
         `<div class="card"><img src="${savedMeme.url}"/></div>`
